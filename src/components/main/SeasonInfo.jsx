2b5f8349f97574faf0f2fef6027000bd96a4db8f
Gen <genesishawkeye42@gmail.com>
2024-05-09 18:26:27 -0300

    FEAT: Added season mock options

--- src/components/main/SeasonInfo.jsx
+++ src/components/main/SeasonInfo.jsx
@@ -1,14 +1,30 @@
 import "./SeasonInfo.scss";
 import CustomNavbar from "../UI/CustomNavbar";
 import Episode from "./Episode";
+import { useState } from "react";
 
 const SeasonInfo = () => {
+  const [activeTab, setActiveTab] = useState("T1");
+
+  const changeTabActive = (seasonId) => {
+    setActiveTab(seasonId);
+  };
+
   const seasons = [1, 2];
-  const seasonsLabels = seasons.map((seasonNumber) => "T" + seasonNumber);
+  const seasonsLabels = seasons.map((seasonNumber) => {
+    return { id: "T" + seasonNumber, labelText: "T" + seasonNumber };
+  });
+
+  console.log(seasonsLabels);
+
   return (
     <section className="season-info">
       <div className="season-info-wrapper">
-        <CustomNavbar labels={seasonsLabels} />
+        <CustomNavbar
+          labels={seasonsLabels}
+          setActiveTab={changeTabActive}
+          activeTab={activeTab}
+        />
         <ul className="season-info__episodes">
           <Episode />
           <Episode />
