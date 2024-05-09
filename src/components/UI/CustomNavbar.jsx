4f21eff567a89f0423c750c76e8e0e026cde38b0
Gen <genesishawkeye42@gmail.com>
2024-05-09 18:25:45 -0300

    FEAT: Added component functionality

--- src/components/UI/CustomNavbar.jsx
+++ src/components/UI/CustomNavbar.jsx
@@ -1,15 +1,23 @@
 import "./CustomNavbar.scss";
 import PropTypes from "prop-types";
 
-const CustomNavbar = ({ labels }) => {
-  // const labels = ["General", "Elenco", "Principais Prêmios"];
+const CustomNavbar = ({ labels, setActiveTab, activeTab }) => {
+  const handleItemClick = (labelId) => {
+    setActiveTab(labelId);
+  };
 
   return (
     <nav className="custom-navbar">
       <ul className="custom-navbar__options">
-        {labels.map((label, index) => (
-          <li key={index} className="custom-navbar__option">
-            {label}
+        {labels.map((label) => (
+          <li
+            key={label.id}
+            className={`custom-navbar__option ${
+              activeTab === label.id ? "active" : ""
+            }`}
+            onClick={() => handleItemClick(label.id)}
+          >
+            {label.labelText}
           </li>
         ))}
       </ul>
@@ -18,7 +26,11 @@ const CustomNavbar = ({ labels }) => {
 };
 
 CustomNavbar.propTypes = {
-  labels: PropTypes.arrayOf(PropTypes.string),
+  labels: PropTypes.arrayOf(
+    PropTypes.shape({ id: PropTypes.string, labelText: PropTypes.string })
+  ).isRequired,
+  setActiveTab: PropTypes.func.isRequired,
+  activeTab: PropTypes.string.isRequired,
 };
 
 export default CustomNavbar;
