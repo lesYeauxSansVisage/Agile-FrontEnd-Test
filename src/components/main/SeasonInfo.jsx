a86795760dae3108a12142609d29cbbfdeeeaa85
Gen <genesishawkeye42@gmail.com>
2024-05-10 12:20:46 -0300

    FEAT: Added data from API

--- src/components/main/SeasonInfo.jsx
+++ src/components/main/SeasonInfo.jsx
@@ -1,44 +1,59 @@
 import "./SeasonInfo.scss";
+import { useState } from "react";
 import CustomNavbar from "../UI/CustomNavbar";
 import Episode from "./Episode";
-import { useState } from "react";
+import { useEpisode } from "../../hooks/UseEpisode";
 
 const SeasonInfo = () => {
-  const [activeTab, setActiveTab] = useState("T1");
+  const { episodeData } = useEpisode();
 
-  const changeTabActive = (seasonId) => {
-    setActiveTab(seasonId);
+  const seasons = {};
+
+  episodeData
+    .sort((a, b) => a && b && b.episodeNumber - a.episodeNumber)
+    .forEach((episode) => {
+      if (!episode) return;
+
+      const season = String(episode.SeasonNumber);
+
+      if (!Object.prototype.hasOwnProperty.call(seasons, season)) {
+        seasons[season] = [];
+      }
+
+      seasons[season].push(episode);
+    });
+
+  const seasonsLabels = Object.keys(seasons).map((season) => ({
+    id: season,
+    label: "T" + season,
+  }));
+
+  const [activeTab, setActiveTab] = useState(seasonsLabels[0].id);
+
+  const changeActiveTab = (id) => {
+    setActiveTab(id);
   };
 
-  const seasons = [1, 2];
-  const seasonsLabels = seasons.map((seasonNumber) => {
-    return { id: "T" + seasonNumber, labelText: "T" + seasonNumber };
-  });
-
-  console.log(seasonsLabels);
+  const currentSeasonEpisodes = seasons[activeTab];
 
   return (
     <section className="season-info">
       <div className="season-info-wrapper">
         <CustomNavbar
           labels={seasonsLabels}
-          setActiveTab={changeTabActive}
+          setActiveTab={changeActiveTab}
           activeTab={activeTab}
         />
         <ul className="season-info__episodes">
-          <Episode />
-          <Episode />
-          <Episode />
-          <Episode />
-          <Episode />
-          <Episode />
-          <Episode />
-          <Episode />
-          <Episode />
-          <Episode />
-          <Episode />
-          <Episode />
-          <Episode />
+          {currentSeasonEpisodes.map((episode) => (
+            <Episode
+              key={episode.ID}
+              episodeNumber={episode.EpisodeNumber}
+              episodeTitle={episode.Title}
+              episodeSynopsis={episode.Synopsis}
+              episodeImage={episode.Image}
+            />
+          ))}
         </ul>
       </div>
     </section>
