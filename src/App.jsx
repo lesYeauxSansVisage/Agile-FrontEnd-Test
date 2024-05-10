7ecfbba6f52ba77c84f5b3eafd659c437e5f3b42
Gen <genesishawkeye42@gmail.com>
2024-05-10 14:36:34 -0300

    FEAT: Added components

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
-import { useContext } from "react";
 import "./App.scss";
-import ShowInfoContext from "./store/show-info-context";
-import exampleData from "./example-data";
-import GeneralOptionButton from "./components/bottom-navbar/GeneralOptionButton";
 import MainHeader from "./components/main/MainHeader";
-import CustomNavbar from "./components/UI/CustomNavbar";
 import SeasonInfo from "./components/main/SeasonInfo";
+import BottomMenu from "./components/bottom-menu/BottomMenu";
+import { useEpisode } from "./hooks/UseEpisode";
+import { useTvShow } from "./hooks/UseTvShow";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
 
 function App() {
-  const { getShowInfo, getEpisodeInfo } = useContext(ShowInfoContext);
+  const { isLoading: isShowLoading } = useEpisode();
+  const { isLoading: isEpisodesLoading } = useTvShow();
 
-  getShowInfo();
-
-  const genres = exampleData.Genres.map(
-    (genre) => genre.Title && genre.Title
-  ).join(" ");
-
-  const bottomNavbarLabels= ["Geral", "Elenco", "Principais Prêmios"]
+  if (isEpisodesLoading || isShowLoading) {
+    return (
+      <div className="loading-container">
+        <LoadingSpinner />
+      </div>
+    );
+  }
 
   return (
     <div className="container">
@@ -27,23 +27,7 @@ function App() {
           <SeasonInfo />
         </div>
 
-        <div className="navbar">
-          <div className="navbar-options-wrapper">
-            <CustomNavbar labels={bottomNavbarLabels} />
-          </div>
-
-          <div className="general__buttons">
-            <GeneralOptionButton buttonLabel="Minha Lista" buttonIcon="a" />
-            <GeneralOptionButton buttonLabel="Avaliar" buttonIcon="a" />
-            <GeneralOptionButton buttonLabel="Gravar" buttonIcon="a" />
-            <GeneralOptionButton buttonLabel="Compartilhar" buttonIcon="a" />
-          </div>
-
-          <div className="synopsis">
-            <h2 className="synopsis__title">Sinopse</h2>
-            <p className="synopsis__text">{exampleData.Synopsis}</p>
-          </div>
-        </div>
+        <BottomMenu />
       </section>
     </div>
   );
