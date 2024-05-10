aedeb1394284228eae7be104536a33cf8fe57478
Gen <genesishawkeye42@gmail.com>
2024-05-10 14:43:34 -0300

    CONFIG: Fixed base path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "'/Agile-FrontEnd-Test/'",
+  base: "/Agile-FrontEnd-Test/",
   plugins: [react()],
 });
