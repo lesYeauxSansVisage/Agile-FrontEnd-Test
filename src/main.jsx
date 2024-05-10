799d5f942e75fba09fdc346ec68a4878b04fff88
Gen <genesishawkeye42@gmail.com>
2024-05-10 14:09:44 -0300

    REFACTOR: Removed Provider

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { ShowInfoProvider } from "./store/show-info-context.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ShowInfoProvider>
-      <App />
-    </ShowInfoProvider>
+    <App />
   </React.StrictMode>
 );
