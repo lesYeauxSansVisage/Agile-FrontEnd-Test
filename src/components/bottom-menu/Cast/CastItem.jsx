1442ae73cff6db7ecce421bd9de21a1455637272
Gen <genesishawkeye42@gmail.com>
2024-05-10 14:08:43 -0300

    FEAT: Added conditional rendering for character name

--- src/components/bottom-menu/Cast/CastItem.jsx
+++ src/components/bottom-menu/Cast/CastItem.jsx
@@ -4,14 +4,14 @@ import PropTypes from "prop-types";
 const CastItem = ({ character, actor }) => {
   return (
     <div className="cast-item">
-      <span className="cast-item__character">{character}</span>
       <span className="cast-item__actor">{actor}</span>
+      {character && <span className="cast-item__character">{actor}</span>}
     </div>
   );
 };
 
 CastItem.propTypes = {
-  character: PropTypes.string.isRequired,
+  character: PropTypes.string,
   actor: PropTypes.string.isRequired,
 };
 
