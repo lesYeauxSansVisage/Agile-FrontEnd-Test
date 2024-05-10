978f8da93793378ff388d26c9ed397f2159f8911
Gen <genesishawkeye42@gmail.com>
2024-05-10 13:40:10 -0300

    FEAT: Added props

--- src/components/main/EpisodeSynopsis.jsx
+++ src/components/main/EpisodeSynopsis.jsx
@@ -1,18 +1,20 @@
 import "./EpisodeSynopsis.scss";
+import PropTypes from "prop-types";
 
-const EpisodeSynopsis = () => {
+const EpisodeSynopsis = ({ synopsis, img }) => {
   return (
     <div className="episode-synopsis">
       <div className="episode-synopsis__img-container">
-        <img src="https://veja.abril.com.br/wp-content/uploads/2017/05/penny-dredful.jpg?quality=90&strip=info&w=750&h=500&crop=1" />
+        <img src={img} />
       </div>
-      <p className="episode-synopsis__text">
-        Londres, 1891. A polícia investiga uma série de assassinatos, mas Sir
-        Malcolm Murray e a bela Vanessa Ives sabem que há algo mais sombrio por
-        trás de tudo.
-      </p>
+      <p className="episode-synopsis__text">{synopsis}</p>
     </div>
   );
+};
+
+EpisodeSynopsis.propTypes = {
+  synopsis: PropTypes.string.isRequired,
+  img: PropTypes.string.isRequired,
 };
 
 export default EpisodeSynopsis;
