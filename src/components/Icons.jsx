82937b4d2723672f02f186d4f5befa58e818a06e
Gen <genesishawkeye42@gmail.com>
2024-05-11 18:08:26 -0300

    FEAT: Added CloseIcon

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -200,3 +200,38 @@ export const RightArrowIcon = () => {
     </svg>
   );
 };
+
+export const CloseIcon = () => {
+  return (
+    <svg
+      viewBox="-0.5 0 25 25"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      stroke="#f7f7f7"
+    >
+      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+      <g
+        id="SVGRepo_tracerCarrier"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></g>
+      <g id="SVGRepo_iconCarrier">
+        {" "}
+        <path
+          d="M3 21.32L21 3.32001"
+          stroke=" #bbbbbb"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        ></path>{" "}
+        <path
+          d="M3 3.32001L21 21.32"
+          stroke=" #bbbbbb"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        ></path>{" "}
+      </g>
+    </svg>
+  );
+};
